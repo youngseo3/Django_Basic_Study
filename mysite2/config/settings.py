bc9261bbcb78e5ab38d2bec55b60ff5770aa5ca5
youngseo3 <wys4475@gmail.com>
2024-07-01 04:26:57 +0900

    Feat: settings.py에서 REST_FRAMEWORK 설정 코드 추가
    권한 클래스, 렌더러 클래스, 파서 클래스 등 DRF의 기본 설정을 구성

--- mysite2/config/settings.py
+++ mysite2/config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from rest_framework import permissions
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,6 +82,16 @@ DATABASES = {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
+}
+# RestFramework
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
+    'DEFAULT_PARSER_CLASSES': [
+        'rest_framework.parsers.JSONParser',
+    ],
 }
 
 
