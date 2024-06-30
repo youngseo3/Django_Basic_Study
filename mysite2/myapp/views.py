8428434f191efd310d51e249ea31d4ae3b565d3e
youngseo3 <wys4475@gmail.com>
2024-06-30 19:42:53 +0900

    Feat: views.py에서 BookSerializer 사용한 create_book으로 수정

--- mysite2/myapp/views.py
+++ mysite2/myapp/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render, redirect
 from .forms import BookForm
 from myapp.models import Book
+from .serializers import BookSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
+@api_view(['POST'])
 def create_book(request):
-    if request.method == 'POST':
-        form = BookForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('book_list')
-    else:
-        form = BookForm()
-    return render(request, 'myapp/create_book.html', {'form': form})
+    serializer = BookSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=201)
+    return Response(serializer.errors, status=400)
 
 def book_list(request):
     books = Book.objects.all()
